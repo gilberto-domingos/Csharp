e8943f793e92b26d308f1636bb33c9de5fb08d8a
gilberto-domingos <domingoshot@hotmail.com>
2025-03-18 15:51:13 -0300

    POO - variaveis privadas (#96561)

--- CursoCsharp/ProgOrientObjeto/Program.cs
+++ CursoCsharp/ProgOrientObjeto/Program.cs
@@ -4,15 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
+        CarrinhoItem tenis = new CarrinhoItem();
+        tenis.Nome = "Tenis de corrida";
+        tenis.Quantidade = 3;
+        tenis.Preco = 999.99M;
+        tenis.ImprimirItens();
         
         CarrinhoItem bola = new CarrinhoItem();
-        bola.nome = "Bola de futebol";
-        bola.quantidade = 1;
-        bola.preco = 99.00M;
-        
+        bola.Nome = "Bola de futebol";
+        bola.Quantidade = 1;
+        bola.Preco = 99.00M;
+        bola.ImprimirItens();
+
         CarrinhoItem camisa = new CarrinhoItem();
-        camisa.nome = "Camisa Polo";
-        camisa.quantidade = 2;
-        camisa.preco = 89.00M;
+        camisa.Nome = "Camisa Polo";
+        camisa.Quantidade = 2;
+        camisa.Preco = 89.00M; 
+        camisa.ImprimirItens();
     }
+    
+    
+    
 }
