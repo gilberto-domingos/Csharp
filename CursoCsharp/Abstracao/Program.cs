f2a5ee468d3382a709c7673a0d76a6847ea3fbcf
gilberto-domingos <domingoshot@hotmail.com>
2025-03-18 17:23:46 -0300

    POO (#96563)

---
+++ CursoCsharp/Abstracao/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Abstracao;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ CursoCsharp/Polimorfismo/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Polimorfismo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
