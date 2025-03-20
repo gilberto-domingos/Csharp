8e7e91067235c0d19bde5a166f2bfd21da283e97
gilberto-domingos <domingoshot@hotmail.com>
2025-03-20 13:14:20 -0300

    POO - Get Set (#96568)

---
+++ ProgOrientadaObjetos/GetSet/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GetSet;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa();
+        p1.Nome = "Roberto";
+        Console.WriteLine(p1.Nome);
+    }
+}
