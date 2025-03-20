697d9dd8e765a85ea3ae29d01934cc797579b0c8
gilberto-domingos <domingoshot@hotmail.com>
2025-03-20 14:40:07 -0300

    POO - Calcular IMC (#96569)

---
+++ ProgOrientadaObjetos/Exerc.IMC/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Security;
+
+namespace Exerc.IMC;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa();
+        p1.Altura = 1.70;
+        p1.Peso = 80.72;
+        double imc1 = p1.calcularImc();
+        Console.WriteLine($"IMC (usando propriedades): {imc1:F2}");
+    }
+}
