1139089b3b8b2f077889796c066de250deb928f7
gilberto-domingos <domingoshot@hotmail.com>
2025-03-23 20:25:31 -0300

    refactor: restruturar rotas (#96571)

--- react/src/routes/routes.tsx
+++ react/src/routes/routes.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Home2 } from "./pages/Home2";
+import { Home } from "../pages/Home";
+import { Home2 } from "../pages/Home2";
 
 export const AppRoutes = () => {
   return (
