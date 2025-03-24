82f871204fe01a3099dd86423a15c0bf8610b6bb
gilberto-domingos <domingoshot@hotmail.com>
2025-03-24 17:22:28 -0300

    feat: implment. Material UI ^6.4.8 (#96574)

--- react/src/pages/Home.tsx
+++ react/src/pages/Home.tsx
@@ -1,10 +1,29 @@
 import { Link } from "react-router-dom";
+import Buttons from "../shared/components/Buttons";
 
 export const Home = () => {
   return (
-    <>
-      Home
-      <Link to="/home2">Home2</Link>
-    </>
+    <div>
+      <h1>Bem-vindo</h1>
+      <Buttons
+        label="Botão Primário"
+        variant="primary"
+        onClick={() => alert("Primário")}
+      />
+      <Buttons
+        label="Botão Secundário"
+        variant="secondary"
+        onClick={() => alert("Secundário")}
+      />
+      <Buttons
+        label="Botão de Perigo"
+        variant="danger"
+        onClick={() => alert("Erro!")}
+      />
+
+      <div style={{ marginTop: "20px" }}>
+        <Link to="/home2">Ir para Home2</Link>
+      </div>
+    </div>
   );
 };
