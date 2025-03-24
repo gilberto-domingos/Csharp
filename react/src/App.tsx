31117881b1654220f98c12101d45ea61cc422a3f
gilberto-domingos <domingoshot@hotmail.com>
2025-03-24 17:22:28 -0300

    instalação config. Material UI ^6.4.8 (#96571)

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,7 +1,15 @@
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { LightTheme } from "./shared/themes";
 import { AppRoutes } from "./routes/routes";
 
 export function App() {
-  return <AppRoutes />;
+  return (
+    <ThemeProvider theme={LightTheme}>
+      <CssBaseline />
+      <AppRoutes />
+    </ThemeProvider>
+  );
 }
 
 export default App;
