aaea9d22dabfe30ea5913e127382d110294b8a69
gilberto-domingos <domingoshot@hotmail.com>
2025-03-25 10:22:15 -0300

    POO - Polimorfismo (#96575)

---
+++ csharp/ProgOrientadaObjetos/Polimorfismo/Imposto.cs
@@ -0,0 +1,15 @@
+namespace Polimorfismo;
+
+public class Imposto
+{
+    public virtual void valeAlimentacao(double salario)
+    {
+       Console.WriteLine("Desconto padrão do vale alimentação R$" + (salario * 0.1));
+       
+    }
+
+    public virtual void valeTransporte(double salario)
+    {
+        Console.WriteLine("Desconto padrão do vale transporte R$" + (salario * 0.06));
+    }
+}
