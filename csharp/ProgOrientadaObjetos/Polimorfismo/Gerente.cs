60c70ddf51174b65220ce3b3fbfd0f5ca7759c5d
gilberto-domingos <domingoshot@hotmail.com>
2025-03-25 10:22:15 -0300

    POO - Polimorfismo (#96572)

---
+++ csharp/ProgOrientadaObjetos/Polimorfismo/Gerente.cs
@@ -0,0 +1,9 @@
+namespace Polimorfismo;
+
+public class Gerente : Imposto
+{
+    public override void valeAlimentacao(double salario)
+    {
+        Console.WriteLine("Desconto gerente do vale alimentação R$" + (salario * 0.15));
+    }
+}
