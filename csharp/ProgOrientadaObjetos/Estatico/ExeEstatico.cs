ca51d04e3b21f0a15b9a18bd30d3d1b9dc410bf8
gilberto-domingos <domingoshot@hotmail.com>
2025-03-27 15:18:11 -0300

    POO - Métodos Estáticos (#96569)

--- csharp/ProgOrientadaObjetos/Estatico/ExeEstatico.cs
+++ csharp/ProgOrientadaObjetos/Estatico/ExeEstatico.cs
@@ -2,5 +2,9 @@ namespace Estatico;
 
 public class ExeEstatico
 {
-    
+    public static string nome = "Alejandro";
+    public static void soma(int n1, int n2)
+    {
+        Console.WriteLine(n1 + n2);
+    }
 }
