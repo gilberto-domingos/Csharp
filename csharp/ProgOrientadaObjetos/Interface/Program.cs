9e6d0c01782988b464df3e56bb3043885b203ecf
gilberto-domingos <domingoshot@hotmail.com>
2025-03-28 19:35:19 -0300

    POO - Interface (#96580)

---
+++ csharp/ProgOrientadaObjetos/Interface/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Interface;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculo calculo = new Calculo();
+        calculo.somar(15,35);
+        calculo.subtrair(85,35);
+        calculo.multiplicar(16,25);
+    }
+}
