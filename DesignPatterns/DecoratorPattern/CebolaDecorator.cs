3a1e96eef26e17c0207194b389eabb4f93c9db37
gilberto-domingos <domingoshot@hotmail.com>
2025-03-29 14:39:47 -0300

    Design Patterns - DecoratorPattern (#96583)

---
+++ DesignPatterns/DecoratorPattern/CebolaDecorator.cs
@@ -0,0 +1,18 @@
+using DecoratorPattern.Interfaces;
+
+namespace DecoratorPattern;
+// Concrete Decorator
+public class CebolaDecorator : PizzaDecorator
+{
+    public CebolaDecorator(IPizza pizza) : base(pizza)
+    {
+        
+    }
+
+    public override string BuscarTipoPizza()
+    {
+        var pizza = base.BuscarTipoPizza();
+        pizza += "\r\n com extra de cebola";
+        return pizza;
+    }
+}
