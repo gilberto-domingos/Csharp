b64a435196648c1b307eec3ca175d30eb0263a17
gilberto-domingos <domingoshot@hotmail.com>
2025-03-29 00:55:35 -0300

    Design Patterns - Factory Method (#96581)

---
+++ csharp/DesignPatterns/FactoryMethod/ConcreteCreator/PlatinumFactory.cs
@@ -0,0 +1,22 @@
+using FactoryMethod.ConcreteProduct;
+using FactoryMethod.Creator;
+using FactoryMethod.Product;
+
+namespace FactoryMethod.ConcreteCreator;
+
+public class PlatinumFactory : CartaoFactory
+{
+    private int _limiteCredito;
+    private int _cobrancaAnual;
+
+    public PlatinumFactory(int limiteCredito, int cobrancaAnual)
+    {
+        this._limiteCredito = limiteCredito;
+        this._cobrancaAnual = cobrancaAnual;
+    }
+    
+    public override CartaoCredito BuscarCartaoCredito()
+    {
+        return new CartaoPlatinum(_limiteCredito, _cobrancaAnual);
+    }
+}
