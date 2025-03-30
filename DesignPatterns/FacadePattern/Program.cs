cfa5134cd2895b341cf480c8b7ae630b4d9343c4
gilberto-domingos <domingoshot@hotmail.com>
2025-03-30 00:50:13 -0300

    Design Patterns - Facade Pattern (#96584)

---
+++ DesignPatterns/FacadePattern/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FacadePattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var facade = new RestauranteFacade();
+        Console.WriteLine("----------------------PEDIDOS DE PIZZA DO CLIENTE----------------------------\n");
+        facade.BuscarPizzaNormal();
+        facade.BuscarPizzaVegana();
+
+        Console.WriteLine("\n----------------------PEDIDOS DE PÃO DO CLIENTE----------------------------\n");
+        facade.BuscarPaoDeAlho();
+        facade.BuscarPaoDeAlhoComQueijo();
+    }
+}
