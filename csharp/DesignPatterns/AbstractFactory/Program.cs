ed98f815f240ccc5e68d9cedaeb3807208553898
gilberto-domingos <domingoshot@hotmail.com>
2025-03-30 16:01:12 -0300

    Design Patterns - Abstract Factrory (#96588)

---
+++ csharp/DesignPatterns/AbstractFactory/Program.cs
@@ -0,0 +1,26 @@
+﻿using AbstractFactory.ConcretesFactory;
+using AbstractFactory.Interfaces;
+
+namespace AbstractFactory;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ITelefoneCelular nokiaTelefone = new Nokia();
+        ClienteTelefoneCelular nokiaCliente = new ClienteTelefoneCelular(nokiaTelefone);
+        
+        Console.WriteLine("*************** NOKIA ***************");
+        Console.WriteLine(nokiaCliente.BuscarSmartPhoneModeloDetalhes());
+        Console.WriteLine(nokiaCliente.BuscarTelefoneNormalModeloDetalhes());
+
+        ITelefoneCelular samsungTelefone = new Samsung();
+        ClienteTelefoneCelular samsungClient = new ClienteTelefoneCelular(samsungTelefone);
+
+        Console.WriteLine("*************** SAMSUNG ***************");
+        Console.WriteLine(samsungClient.BuscarSmartPhoneModeloDetalhes());
+        Console.WriteLine(samsungClient.BuscarTelefoneNormalModeloDetalhes());
+
+        Console.ReadKey();
+    }
+}
