98018042d41ce13711a3e761eb71b937a384270f
gilberto-domingos <domingoshot@hotmail.com>
2025-04-03 14:25:05 -0300

    refactor: refatoração com variáveis privadas e expressões lambada (#96597)

--- SOLID/SingleResponsibilityPrinciple/Report.cs
+++ SOLID/SingleResponsibilityPrinciple/Report.cs
@@ -1,21 +1,36 @@
-namespace SingleResponsibilityPrinciple
+namespace SingleResponsibilityPrinciple;
+
+public class Report
 {
-    public class Report
+    private string _title;
+    private string _content;
+
+    public Report(string title, string content)
     {
-        public string Title { get; set; }
-        public string Content { get; set; }
+        Title = title;
+        Content = content;
+    }
 
-        public Report(string title, string content)
-        {
-            Title = title;
-            Content = content;
-        }
+    public string Title
+    {
+        get => _title;
+        private set => _title = string.IsNullOrWhiteSpace(value) 
+            ? throw new ArgumentException("O título do relatório não pode ser vazio.") 
+            : value;
+    }
 
-        public void Generate()
-        {
-            Console.WriteLine("Gerando Relatório...");
-            Console.WriteLine($"Título: {Title}");
-            Console.WriteLine($"Conteúdo: {Content}");
-        }
+    public string Content
+    {
+        get => _content;
+        private set => _content = string.IsNullOrWhiteSpace(value) 
+            ? throw new ArgumentException("O conteúdo do relatório não pode ser vazio.") 
+            : value;
+    }
+
+    public void Generate()
+    {
+        Console.WriteLine("Gerando relatório...");
+        Console.WriteLine($"Título: {Title}");
+        Console.WriteLine($"Conteúdo: {Content}");
     }
 }
