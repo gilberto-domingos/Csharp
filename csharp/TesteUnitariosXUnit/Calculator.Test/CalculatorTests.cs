61bfc2c29c3e303dfeba56fa6dc6ccb9b23c7577
gilberto-domingos <domingoshot@hotmail.com>
2025-04-11 15:24:54 -0300

    Teste Unitario XUnit ok (#26478)

---
+++ csharp/TesteUnitariosXUnit/Calculator.Test/CalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Calculator.Test;
+
+public class CalculatorTests
+{
+    [Fact(DisplayName = "When sum should success.")]
+    public void WhenSum_Sucess()
+    {
+        //arrane
+        const int resultExpect = 138;
+        const int a = 56;
+        const int b = 82;
+        
+        // act
+        var resultActual  = Calculator.Sum(a, b);
+        
+        Assert.Equal(resultExpect, resultActual); 
+    }
+}
