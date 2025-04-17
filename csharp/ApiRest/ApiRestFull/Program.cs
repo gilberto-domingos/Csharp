9bcfe161ba02b6adf476e28a7a97f09669626693
gilberto-domingos <domingoshot@hotmail.com>
2025-04-17 01:41:11 -0300

    Configuração do Swagger (#99815)

--- csharp/ApiRest/ApiRestFull/Program.cs
+++ csharp/ApiRest/ApiRestFull/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using SistemaVendas.Data;
 using SistemaVendas.Interfaces;
 using SistemaVendas.Services;
-
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,11 +20,28 @@ builder.Configuration["ConnectionStrings:DefaultConnection"] = connectionString;
 builder.WebHost.UseUrls("http://0.0.0.0:5000");
 
 builder.Services.AddScoped<HelloService>();
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services.AddScoped<IAutor, AutorService>();
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "ApiRestFull",
+        Version = "v1",
+        Description = "Documentação da ApiRestFull",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Name = "gilberto domingos jr",
+            Email = "jrdomingosjr00@gmail.com",
+        }
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddDbContext<ApiDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -34,7 +51,15 @@ var app = builder.Build();
 app.UseRouting();
 
 app.UseHttpsRedirection();
+
 app.UseAuthorization();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiRestFull v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.MapControllers();
 
