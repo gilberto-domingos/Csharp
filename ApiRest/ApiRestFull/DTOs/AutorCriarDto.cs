e6fcf31dfcb3952acd74dd74d036fa3517731019
gilberto-domingos <domingoshot@hotmail.com>
2025-04-26 14:15:56 -0300

    refactor: trocar annotation por palavra-chave(#96643)

--- ApiRest/ApiRestFull/DTOs/AutorCriarDto.cs
+++ ApiRest/ApiRestFull/DTOs/AutorCriarDto.cs
@@ -4,9 +4,7 @@ namespace ApiRestFull.DTOs;
 
 public class AutorCriarDto
 {
-    [Required]
-    public string Nome { get; set; } = string.Empty;
+    public required string Nome { get; set; } = string.Empty;
 
-    [Required]
-    public string Sobrenome { get; set; } = string.Empty;
+    public required string Sobrenome { get; set; } = string.Empty;
 }
--- ApiRest/ApiRestFull/DTOs/AutorEditarDto.cs
+++ ApiRest/ApiRestFull/DTOs/AutorEditarDto.cs
@@ -6,9 +6,7 @@ public class AutorEditarDto
 {
     public Guid idAutor { get; init; }
 
-    [Required]
-    public string Nome { get; set; } = string.Empty;
+    public required string Nome { get; set; } = string.Empty;
 
-    [Required]
-    public string Sobrenome { get; set; } = string.Empty;
+    public required string Sobrenome { get; set; } = string.Empty;
 }
