acb77f196da74ce6e12541a8a6ffbcc86bddaff8
gilberto-domingos <domingoshot@hotmail.com>
2025-04-26 14:15:56 -0300

    refactor: trocar annotation por palavra-chave(#96643)

--- csharp/ApiRest/ApiRestFull/DTOs/AutorVinculoDto.cs
+++ csharp/ApiRest/ApiRestFull/DTOs/AutorVinculoDto.cs
@@ -7,9 +7,7 @@ public class AutorVinculoDto
     [Key]
     public Guid IdAutor { get; init; }
 
-    [Required]
     public required string Nome { get; set; } = string.Empty;
 
-    [Required]
     public required string Sobrenome { get; set; } = string.Empty;
 }
