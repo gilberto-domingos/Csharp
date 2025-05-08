0d9916ff3d4dcf6769b10f287514dfc26491127e
gilberto-domingos <domingoshot@hotmail.com>
2025-05-08 17:39:06 -0300

    refactor: ajustar migrations e context (#96677)

--- csharp/LaboratorioDeTestesAPI/Migrations/20250508172939_AjustesModelos.Designer.cs
+++ csharp/LaboratorioDeTestesAPI/Migrations/20250508172939_AjustesModelos.Designer.cs
@@ -4,6 +4,7 @@ using LaboratorioDeTestesAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LaboratorioDeTestesAPI.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250508172939_AjustesModelos")]
+    partial class AjustesModelos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
