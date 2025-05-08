b4f4cb252dc653fb540010c6a14a51d1b999d9e0
gilberto-domingos <domingoshot@hotmail.com>
2025-05-08 17:37:15 -0300

    refactor: registrar serviços,interfaces e impl. serilog (#96676)

--- csharp/LaboratorioDeTestesAPI/Program.cs
+++ csharp/LaboratorioDeTestesAPI/Program.cs
@@ -1,13 +1,26 @@
 using LaboratorioDeTestesAPI.Data;
-using Microsoft.EntityFrameworkCore;
 using LaboratorioDeTestesAPI.Interfaces;
-using LaboratorioDeTestesAPI.Services;
 using LaboratorioDeTestesAPI.Repositories;
+using LaboratorioDeTestesAPI.Services;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Serilog;
 using System.Reflection;
+
+DotNetEnv.Env.Load();
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .Enrich.FromLogContext()
+    .WriteTo.Console(
+        outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}",
+        theme: Serilog.Sinks.SystemConsole.Themes.AnsiConsoleTheme.Code)
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 
-DotNetEnv.Env.Load();
+builder.Host.UseSerilog();
 
 var dbServer = Environment.GetEnvironmentVariable("DB_SERVER");
 var dbName = Environment.GetEnvironmentVariable("DB_NAME");
@@ -20,34 +33,36 @@ builder.Configuration["ConnectionStrings:DefaultConnection"] = connectionString;
 
 builder.WebHost.UseUrls("http://0.0.0.0:5000");
 
+builder.Services.AddScoped<IAutor, AutorService>();
+builder.Services.AddScoped<IAutorRepository, AutorRepository>();
+builder.Services.AddScoped<ILivro, LivroService>();
+builder.Services.AddScoped<ILivroRepository, LivroRepository>();
 builder.Services.AddScoped<ICar, CarChassiValidatorService>();
 builder.Services.AddScoped<ICarRepository, CarRepository>();
 
-
-
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
 builder.Services.AddDbContext<ApiDbContext>(options =>
     options
-        .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-        .LogTo(Console.WriteLine, LogLevel.Information) // só para desenvolvimento
-        .EnableSensitiveDataLogging());
+        .UseSqlServer(connectionString)
+        .LogTo(Log.Logger.Information, LogLevel.Information)
+        .EnableSensitiveDataLogging(false)
+        .ConfigureWarnings(warnings =>
+            warnings.Ignore(CoreEventId.SensitiveDataLoggingEnabledWarning)));
 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("ReactPolicy", policy =>
     {
         policy.WithOrigins("http://localhost:5173")
-            .AllowAnyHeader()
-            .AllowAnyMethod();
+              .AllowAnyHeader()
+              .AllowAnyMethod();
     });
 });
 
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 
 var app = builder.Build();
 
@@ -57,14 +72,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
-
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<ApiDbContext>();
     ApiDbContext.Initialize(scope.ServiceProvider, context);
 }
 
+app.UseCors("ReactPolicy");
 
 app.MapControllers();
 
