a74f8dc56b4efa3dd812cc63827a53995596ff4f
gilberto-domingos <domingoshot@hotmail.com>
2025-05-09 23:12:32 -0300

    feat: criar projeto de testes (#96686)

---
+++ csharp/LaboratorioDeTestesAPI/LaboratorioDeTestes.Tests/Entities/CarTests.cs
@@ -0,0 +1,10 @@
+namespace LaboratorioDeTestes.Tests.Entities;
+
+public sealed class CarTests
+{
+    [Fact]
+    public void Constructor_GivenAllParameters_ThenShouldSetThePropertiesCorrectly()
+    {
+
+    }
+}
