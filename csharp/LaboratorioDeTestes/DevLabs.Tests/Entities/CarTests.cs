5f08d2999a8822bc3c170a4f3a19fb0a5866ad88
gilberto-domingos <domingoshot@hotmail.com>
2025-05-11 14:45:03 -0300

    feat: teste untitário com package Faker (#96698)

--- csharp/LaboratorioDeTestes/DevLabs.Tests/Entities/CarTests.cs
+++ csharp/LaboratorioDeTestes/DevLabs.Tests/Entities/CarTests.cs
@@ -2,35 +2,47 @@ using Bogus;
 
 
 using DevLabs.Api.Entities;
+using Xunit.Abstractions;
 
 namespace DevLabs.Tests.Entities;
 
 public sealed class CarTests
 {
     private readonly Faker _faker = new Faker("pt_BR");
+    private readonly ITestOutputHelper _testOutputHelper;
 
+    public CarTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
 
     [Fact]
-    public void Constructor_GivenAllParameters_ThenShouldSetThePropertiesCorrectly3()
+    public void BogusConstructor_GivenAllParameters_ThenShouldSetThePropertiesCorrectly()
     {
         // BOGUS
         //Arr - GivenAllParameters
         var expectedId = Guid.NewGuid();
         var expectedName = _faker.Vehicle.Model();
+        var expectedChassi = _faker.Random.Int();
+
+        _testOutputHelper.WriteLine(expectedName); // se precisar immprimir na console
+        // dotnet test --logger:"console;verbosity=detailed"
 
         //Act
-        var car = new Car(expectedId, expectedName);
+        var car = new Car(expectedId, expectedName, expectedChassi);
 
         //Assert - ShouldSetThePropertiesCorrectly
         Assert.Equal(expectedId, car.Id);
         Assert.Equal(expectedName, car.Name);
+        Assert.Equal(expectedChassi, car.Chassi);
     }
 
 
 
     [Theory]
     [InlineData("Ferrari")]
-    public void Constructor_GivenAllParameters_ThenShouldSetThePropertiesCorrectly2(string expectedName)
+    [InlineData("Fusca ")]
+    public void TheoryConstructor_GivenAllParameters_ThenShouldSetThePropertiesCorrectly(string expectedName)
     {
         //Arr - GivenAllParameters
         var expectedId = Guid.NewGuid();
