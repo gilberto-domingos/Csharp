8ec43f3bc2444b0d55cc50dc600d0447050720d1
gilberto-domingos <domingoshot@hotmail.com>
2025-05-20 10:52:54 -0300

    test(Autor) adiciona testes no Autor (#96728)

--- LaboratorioDeTestes/DevLabs.Tests/Entities/AutorTests.cs
+++ LaboratorioDeTestes/DevLabs.Tests/Entities/AutorTests.cs
@@ -1,6 +1,71 @@
-namespace DevLabs.Tests.Entities;
+    using Bogus;
+    using DevLabs.Api.Entities;
+    using FluentAssertions;
 
-public class AutorTests
-{
+    namespace DevLabs.Tests.Entities;
+
+    [Trait("Entidade", "Autor")]
+    [Trait("Tipo", "Unitario")]
+    public class AutorTests
+    {
+        private readonly Faker _faker = new Faker("pt_BR");
+        
+
+        [Fact]
+        public void Construtor_DeveCriarAutorComDadosValidos()
+        {
+            var expectedId = Guid.NewGuid();
+            var expectedNome = _faker.Name.FirstName();
+            var expectedSobrenome = _faker.Name.LastName();
+
+            var autorFake = new Autor(expectedNome, expectedSobrenome, new List<Livro>());
+
+            var livroFaker = new Faker<Livro>("pt_BR")
+                .CustomInstantiator(f => new Livro(
+                    f.Random.Guid(),
+                    f.Lorem.Sentence(3),
+                    autorFake.IdAutor,
+                    autorFake
+                ));
+
+            var expectedLivros = livroFaker.Generate(2); 
+
+            var autor = new Autor(expectedId, expectedNome, expectedSobrenome, expectedLivros);
+
+            autor.IdAutor.Should().Be(expectedId);
+            autor.Nome.Should().Be(expectedNome);
+            autor.Sobrenome.Should().Be(expectedSobrenome);
+            autor.Livros.Should().BeEquivalentTo(expectedLivros);
+        }
+
+        [Fact]
+        public void Construtor_DeveLancarArgumentNullExceptionSeNomeForNulo()
+        {
+            var expectedId = Guid.NewGuid();
+            string? expectedNome = null;
+            var expectedSobrenome = _faker.Name.LastName();
+
+            Action act = () => new Autor(expectedId, expectedNome!, expectedSobrenome, new List<Livro>());
+
+            act.Should()
+                .Throw<ArgumentNullException>()
+                .Where(e => e.ParamName == "nome");
+        }
+
+        [Fact]
+        public void Construtor_DeveLancarArgumentNullExceptionSeSobrenomeForNulo()
+        {
+            var expectedId = Guid.NewGuid();
+            var expectedNome = _faker.Name.FirstName();
+            string? expectedSobrenome = null;
+
+            Action act = () => new Autor(expectedId, expectedNome, expectedSobrenome!, new List<Livro>());
+
+            act.Should()
+                .Throw<ArgumentNullException>()
+                .Where(e => e.ParamName == "sobrenome");
+        }
+    }
+
+
     
-}
