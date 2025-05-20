24b869ab61f2be9bcb76c572b8ab1211b6b3a96f
gilberto-domingos <domingoshot@hotmail.com>
2025-05-20 10:48:05 -0300

    refactor: remove espaço (#96726)

--- LaboratorioDeTestes/DevLabs.Api/Entities/Autor.cs
+++ LaboratorioDeTestes/DevLabs.Api/Entities/Autor.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Serialization;
 
 namespace DevLabs.Api.Entities;
@@ -7,28 +8,29 @@ public class Autor
 {
     [Key]
     public Guid IdAutor { get; init; }
-
-    public required string Nome { get; set; }
-
-    public required string Sobrenome { get; set; }
+    public required string Nome { get; set; } 
+    public required string Sobrenome { get; set; } 
 
     [JsonIgnore]
     public ICollection<Livro> Livros { get; set; } = new List<Livro>();
-
     public Autor() { }
 
-    public Autor(string nome, string sobrenome)
+    [SetsRequiredMembers]
+    public Autor(string nome, string sobrenome, ICollection<Livro> livros)
     {
         IdAutor = Guid.NewGuid();
         Nome = nome ?? throw new ArgumentNullException(nameof(nome));
         Sobrenome = sobrenome ?? throw new ArgumentNullException(nameof(sobrenome));
+        Livros = livros ?? throw new ArgumentNullException(nameof(livros));
     }
 
     //Const.extra para testes automatizados, testes unitários, mocks ou imports com ID definido
-    public Autor(Guid idAutor, string nome, string sobrenome)
+    [SetsRequiredMembers]
+    public Autor(Guid idAutor, string nome, string sobrenome, ICollection<Livro> livros)
     {
         IdAutor = idAutor;
         Nome = nome ?? throw new ArgumentNullException(nameof(nome));
         Sobrenome = sobrenome ?? throw new ArgumentNullException(nameof(sobrenome));
+        Livros = livros ?? throw new ArgumentNullException(nameof(livros));
     }
 }
