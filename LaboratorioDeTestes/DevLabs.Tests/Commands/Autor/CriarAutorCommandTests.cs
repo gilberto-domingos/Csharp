33e056aa5956fe8fcd5c01f4be50f1e8c53fea30
gilberto-domingos <domingoshot@hotmail.com>
2025-05-22 23:18:20 -0300

    tests(CriarAutorCommand): gerar dto valido (#96757)

--- LaboratorioDeTestes/DevLabs.Tests/Commands/Autor/CriarAutorCommandTests.cs
+++ LaboratorioDeTestes/DevLabs.Tests/Commands/Autor/CriarAutorCommandTests.cs
@@ -2,7 +2,6 @@ using Bogus;
 using DevLabs.Api.Commands.Autor;
 using DevLabs.Api.Dtos;
 using FluentAssertions;
-using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace DevLabs.Tests.Commands.Autor;
 
@@ -12,20 +11,41 @@ namespace DevLabs.Tests.Commands.Autor;
     public class CriarAutorCommandTests
     {
         private readonly Faker _faker = new();
+        // campos são required precisa inicializar via object initializer
+        private LivroCriarDto GerarLivroDtoValido()
+        {
+            var autorId = Guid.NewGuid();
+    
+            return new LivroCriarDto
+            {
+                Titulo = _faker.Lorem.Sentence(),
+                IdAutor = autorId,
+                Autor = new AutorVinculoDto
+                {
+                    IdAutor = autorId,
+                    Nome = _faker.Person.FirstName,
+                    Sobrenome = _faker.Person.LastName
+                }
+            };
+        }
         
-        private AutorCriarDto GerarDtoValido()
+        private AutorCriarDto GerarAutorDtoValido()
         {
             return new AutorCriarDto
             {
                 Nome = _faker.Person.FirstName,
-                Sobrenome = _faker.Person.LastName
+                Sobrenome = _faker.Person.LastName,
+                Livros = new List<LivroCriarDto>
+                {
+                    GerarLivroDtoValido()
+                }
             };
         }
         
         [Fact]
         public void Deve_Criar_Comando_Valido()
         {
-            var dto = GerarDtoValido();
+            var dto = GerarAutorDtoValido();
 
             var command = new CriarAutorCommand(dto);
 
@@ -36,7 +56,7 @@ namespace DevLabs.Tests.Commands.Autor;
         [Fact]
         public void Deve_Conter_Dto_Equivalente_Ao_Passado()
         {
-            var dto = GerarDtoValido();
+            var dto = GerarAutorDtoValido();
 
             var command = new CriarAutorCommand(dto);
 
