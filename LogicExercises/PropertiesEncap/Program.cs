7ae897c6f46189e93ceaad55eb7f1623e5fde7cb
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-07-01 14:35:42 -0300

    chore: atualiza exercicios de lógica (#96794)

--- LogicExercises/PropertiesEncap/Program.cs
+++ LogicExercises/PropertiesEncap/Program.cs
@@ -6,24 +6,24 @@ class Program
 {
     static void Main()
     {
-        // Student gradeStudent1 = new Student();
-        //
-        // Console.Write("Digite o nome do aluno:");
-        // gradeStudent1.Name = Console.ReadLine();
-        //
-        // Console.Write("Digite a nota do aluno (0 a 10): ");
-        // gradeStudent1.Grade = double.Parse(Console.ReadLine());
+        Student gradeStudent1 = new Student();
+        
+        Console.Write("Digite o nome do aluno:");
+        gradeStudent1.Name = Console.ReadLine();
+        
+        Console.Write("Digite a nota do aluno (0 a 10): ");
+        gradeStudent1.Grade = double.Parse(Console.ReadLine());
 
-    //     try
-    //     {
-    //         //User user1 = new User("i9u90u9u09u*8");
-    //         User user1 = new User("i*8");
-    //         Console.WriteLine(user1.Password);
-    //     }
-    //     catch(InvalidOperationException ex)
-    //     {
-    //        Console.WriteLine($"Erro: {ex.Message}"); 
-    //     }
+        try
+        {
+            //User user1 = new User("i9u90u9u09u*8");
+            User user1 = new User("i*8");
+            Console.WriteLine(user1.Password);
+        }
+        catch(InvalidOperationException ex)
+        {
+           Console.WriteLine($"Erro: {ex.Message}"); 
+        }
 
     Employee emp1 = new Employee();
     Console.WriteLine("Digite o salário :");
