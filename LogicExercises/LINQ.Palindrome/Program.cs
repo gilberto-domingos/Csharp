06df6689b2274f3962cdc455445455ed569de7d3
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-09-04 03:21:16 -0300

    feat: palindromo link (#96805)

---
+++ LogicExercises/LINQ.Palindrome/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Palindrome
+{
+    public static bool isAlphaPalindrome(string code)
+    {
+        string filtered = new string(code
+            .Where(char.IsLetter)
+            .Select(char.ToLower)
+            .ToArray());
+
+        return filtered.SequenceEqual(filtered.Reverse());
+    }
+    
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um palindromo");
+        string input = Console.ReadLine();
+        bool result = isAlphaPalindrome(input);
+        Console.WriteLine(result ? 1 : 0);
+    }
+}
