5ebca0760648cb674ac3111dd9d376f80b68b99d
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-09-04 03:23:13 -0300

    feat: range spread com arrays (#96806)

---
+++ LogicExercises/ArrayRangeSpread/Program.cs
@@ -0,0 +1,23 @@
+﻿class ArraysClass
+{
+    public IEnumerable<int> ReadSequence()
+    {
+        int[] numbers = { 1, 2, 3 };
+        IEnumerable<int> moreNumbers = [..numbers, 4, 5, 6, 7, 8, 9, 10];
+
+        return moreNumbers;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArraysClass arrays = new ArraysClass();
+
+        foreach (var n in arrays.ReadSequence())
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
