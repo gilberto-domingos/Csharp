ceca7715e8410b59c0acb09a2bf65137627e1178
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-09-05 05:00:45 -0300

    feat: Senac nivelamento exec 1 e 2 (#96809)

---
+++ LogicExercises/EvenNumbers/Program.cs
@@ -0,0 +1,57 @@
+﻿
+/*2- Escreva um programa que leia dois valores inteiros e encontre todos os números pares entre eles.
+// a. Considerar somente valores positivos
+// b. O segundo número lido não pode ser menor ou igual ao primeiro
+// c. Caso o usuário digite um valor inválido, deve ser permitido uma
+// nova digitação.*/
+
+class SolveProblem
+{
+    public int num1, num2;
+
+     public void ReceiveValues()
+     {
+
+          while (true)
+          {
+               Console.WriteLine("Digite o primeiro número (positivo)");
+               num1 = int.Parse(Console.ReadLine());
+
+               if (num1 > 0) 
+                    break;
+               
+              Console.WriteLine(" O número precisa ser positivo. Tente novamente.\n");
+          }
+
+          while (true)
+          {
+               Console.WriteLine("Digite o segundo número (maior que o primeiro e positivo):");
+               num2 = int.Parse(Console.ReadLine());
+
+               if (num2 > 0 && num2 > num1) 
+                    break;
+               
+              Console.WriteLine("O número precisa ser maior que o primeiro e positivo. Tente novamente.\n");   
+          }    
+          
+               
+          Console.WriteLine($"Números pares entre {num1} e {num2}:");
+
+          for (int i = num1; i <= num2; i++)
+          {
+               if(i % 2 == 0)
+                    Console.WriteLine(i);
+          }
+          
+          Console.WriteLine();
+     }
+}
+
+class Program
+{
+     static void Main(string[] args)
+     {
+          var solveProblem = new SolveProblem();
+          solveProblem.ReceiveValues();
+     }
+}
---
+++ LogicExercises/PrintNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿// 1- Escreva um programa para exibir os números de 1 até 50 na tela.
+
+class SolveProblem
+{
+    public void printNumbers()
+    {
+        for (int i = 1; i <= 50; i++)
+        {
+            Console.WriteLine($"Número:{i}");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var solveProblem = new SolveProblem();
+        solveProblem.printNumbers();
+    }
+}
