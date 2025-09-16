6aba45d53bc279e3746a307f01c9c393c3edde1f
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-09-16 18:08:50 -0300

    feat: Senac tabuada com For (#96820)

---
+++ LogicExercises/MultiplyFor/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+           Console.WriteLine("Digite um número inteiro para ver a tabuada.");
+           string input = Console.ReadLine();
+
+           bool validade = int.TryParse(input, out int number);
+
+           if (validade)
+           {
+               for (int i = 0; i <= 10; i++)
+               {
+                   Console.WriteLine($"{number} x {i} = {number * i}");
+               }
+           }
+           else
+           {
+               Console.WriteLine("Entrada invalida, digite um número inteiro.");
+           }
+        }
+        catch (Exception ex)
+        {
+            throw new ArgumentException("Ocorreu um erro no sistema", ex.Message);
+        }
+    }
+}
