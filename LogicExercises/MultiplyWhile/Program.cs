fb45e0bd6b7f694f56681a6123990a7579f0476a
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-09-16 18:18:22 -0300

    feat: Senac tabuada com While (#96821)

---
+++ LogicExercises/MultiplyWhile/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Digite um número inteiro para ver a tabuada.");
+            string input = Console.ReadLine();
+
+            bool validate = int.TryParse(input, out int number);
+
+            if (validate)
+            {
+                int i = 1;
+                while (i <= 10)
+                {
+                    Console.WriteLine($"{number} x {i} = {number * i}");
+                    i++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida digite um número inteiro.");
+            }
+
+        }
+        catch (Exception ex)
+        {
+            throw new ArgumentException("Ocorreu um problema no sistema", ex.Message);
+        }
+    }
+}
