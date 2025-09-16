e6f59a1b01f82e6a7356a988c59a76ba272b5d63
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-09-16 18:26:19 -0300

    feat: Senac tabuada com DoWhile (#96822)

---
+++ LogicExercises/MultiplyDoWhile/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+           Console.WriteLine("Digite um número inteiro para ver a tabuada.");
+           string input = Console.ReadLine();
+
+           bool validate = int.TryParse(input, out int number);
+
+           if (validate)
+           {
+               int i = 1;
+               do
+               {
+                   Console.WriteLine($"{number} x {i} = {number * i}");
+                   i++;
+               } while (i <= 10);
+           }
+           else
+           {
+               Console.WriteLine("Entrada inválida, digite um número inteiro");
+           }
+        }
+        catch (Exception ex)
+        {
+            throw new ArgumentException("Ocorreu um erro no sistemaa", ex.Message);
+        }
+    }
+}
